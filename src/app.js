9fc86089c6cdf16ddf30c38c0916a81e6f7af173
Jefte <jefte199@gmail.com>
2020-09-03 18:11:14 -0300

    second commit heroku

--- src/app.js
+++ src/app.js
@@ -1,37 +1,11 @@
 import express from 'express';
 import path from 'path';
-import { Router } from 'express';
-import api from './helpers/api';
+import routes from './routes';
 
-
-const routes = new Router();
 const app = express();
 
 
 
-routes.get('/', (req, res) => {
-    return res.render('index');
-});
-
-routes.get('/clientes', (req, res) => {
-    return res.render('clientes');
-});
-
-routes.get('/portifolio', (req, res) => {
-
-    const { search } = req.query;
-
-    api.get('/', {
-        params: {
-            user_id: '17841407894545225',
-            q: search
-        }
-    }).then(({ data }) => res.render('portifolio', { img: data.data }))
-});
-
-routes.get('/contato', (req, res) => {
-    return res.render('contato');
-});
 
 
 app.use(routes);
---
+++ src/routes.js
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import api from './helpers/api';
+
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+  return res.render('index');
+});
+
+routes.get('/clientes', (req, res) => {
+  return res.render('clientes');
+});
+
+routes.get('/portifolio', (req, res) => {
+
+  const { search } = req.query;
+
+  api.get('/', {
+      params: {
+          user_id: '17841407894545225',
+          q: search
+      }
+  }).then(({ data }) => res.render('portifolio', { img: data.data }))
+});
+
+routes.get('/contato', (req, res) => {
+  return res.render('contato');
+});
+
+export default routes;
